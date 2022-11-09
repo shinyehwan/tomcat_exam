5a0a40c3563bfe6ae200ead3a53b02656a5355c9
shinyehwan <jghisjl007@naver.com>
2022-11-09 21:24:42 +0900

    req와 resp를 직접 다루기 귀찮으니, Rq 도입

---
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -0,0 +1,30 @@
+package com.ll.exam;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Rq rq = new Rq(req, resp);
+
+
+		// int dan = Integer.parseInt(req.getParameter("dan"));
+		// int limit = Integer.parseInt(req.getParameter("limit"));
+		int dan = rq.getIntParam("dan", 0);
+		int limit = rq.getIntParam("limit", 0);
+
+		// resp.getWriter().append("<h1>%d단</h1>\n".formatted(dan));
+		rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
+		for (int i = 1; i <= limit; i++) {
+			// resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
+			rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
+		}
+	}
+}
