4b2cdea09029dfd517f79b460a516b7475554ec2
shinyehwan <jghisjl007@naver.com>
2022-11-09 22:07:40 +0900

    JSP 방식으로 구구단 구현

--- src/main/java/com/ll/exam/GugudanServlet.java
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -17,8 +17,8 @@ public class GugudanServlet extends HttpServlet {
 
 		// int dan = Integer.parseInt(req.getParameter("dan"));
 		// int limit = Integer.parseInt(req.getParameter("limit"));
-		int dan = rq.getIntParam("dan", 0);
-		int limit = rq.getIntParam("limit", 0);
+		int dan = rq.getIntParam("dan", 9);
+		int limit = rq.getIntParam("limit", 9);
 
 		// resp.getWriter().append("<h1>%d단</h1>\n".formatted(dan));
 		rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
